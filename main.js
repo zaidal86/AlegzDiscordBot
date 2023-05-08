58022482ce08ea2bdb8f4361d4a7ca9a5fc6f8de
Debian <debian@vps-efa81030.vps.ovh.net>
2023-05-08 02:27:35 +0000

    Add slash command

--- main.js
+++ main.js
@@ -4,16 +4,38 @@ import { headsOrTails } from './commands/heads-or-tails.js';
 import { currentPubMap } from './commands/apex.js';
 import { autoSchedule } from './commands/auto.js';
 
+const IDServer = '439897141025046532';
+
 dotenv.config();
 
 const bot = new Eris(process.env.BOT_TOKEN);
 
-bot.on('ready', () => {
+bot.on('ready', async () => {
     console.log('Ready!');
+    await bot.createGuildCommand(IDServer, {
+        name: 'pof',
+        type: 1,
+        description: 'headsOrTails',
+      });
+    await bot.createGuildCommand(IDServer, {
+        name: 'plouf',
+        type: 1,
+        description: 'plouf',
+      });
+    await bot.createGuildCommand(IDServer, {
+        name: 'auto',
+        type: 1,
+        description: 'auto horaire de travail',
+      });
+    await bot.createGuildCommand(IDServer, {
+        name: 'rota',
+        type: 1,
+        description: 'map rotation',
+      });
 });
 
-bot.on('messageCreate', (msg) => {
-    console.log(msg);
+bot.on('messageCreate', async (msg) => {
+    // console.log(msg);
     console.log(msg.channel.id);
 
     switch (msg.content) {
@@ -27,12 +49,32 @@ bot.on('messageCreate', (msg) => {
             );
             break;
         case '!rota':
-            bot.createMessage(msg.channel.id, currentPubMap());
+            bot.createMessage(msg.channel.id, await currentPubMap());
             break;
         case '!auto':
             bot.createMessage(msg.channel.id, autoSchedule());
             break;
     }
 });
+
+bot.on('interactionCreate', async (Interaction) => {
+    switch (Interaction.data.name) {
+        case 'pof':
+            Interaction.createMessage(msg.channel.id, headsOrTails());
+            break;
+        case 'plouf':
+            Interaction.createMessage(
+                msg.channel.id,
+                'Cela va au delà de mes compétences.'
+            );
+            break;
+        case 'rota':
+            Interaction.createMessage(msg.channel.id, await currentPubMap());
+            break;
+        case 'auto':
+            Interaction.createMessage(msg.channel.id, autoSchedule());
+            break;
+        }
+  });
 
 bot.connect();
