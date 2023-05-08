577732bf74310112efa3582c3583914f1bec64b5
Alexandre Farrenq <alexandre1.farrenq@epitech.eu>
2023-05-08 16:49:58 +0000

    Update auto schedule command

--- src/commands/auto.ts
+++ src/commands/auto.ts
@@ -1,6 +1,7 @@
 export const autoSchedule = (): string => {
     const startDate: Date = new Date('2023-05-06');
     const today: Date = new Date();
+    const timezoneOffset: number = today.getTimezoneOffset() / 60;
 
     const daysDifference: number =
         Math.floor(
@@ -10,42 +11,19 @@ export const autoSchedule = (): string => {
     let schedule: string;
 
     if (daysDifference >= 0 && daysDifference < 2) {
-        schedule = "Aujourd'hui, auto travaille de 5h à 13h.";
+        schedule = `Aujourd'hui, auto travaille de ${5 + timezoneOffset}h à ${
+            13 + timezoneOffset
+        }h.`;
     } else if (daysDifference >= 2 && daysDifference < 4) {
-        schedule = "Aujourd'hui, auto travaille de 13h à 21h.";
+        schedule = `Aujourd'hui, auto travaille de ${13 + timezoneOffset}h à ${
+            21 + timezoneOffset
+        }h.`;
     } else if (daysDifference >= 4 && daysDifference < 6) {
-        schedule = "Aujourd'hui, auto travaille de 21h à 5h.";
+        schedule = `Aujourd'hui, auto travaille de ${21 + timezoneOffset}h à ${
+            5 + timezoneOffset
+        }h.`;
     } else {
         schedule = "Aujourd'hui, auto ne travaille pas.";
-    }
-
-    const workStart: Date = new Date(today);
-    const workEnd: Date = new Date(today);
-    workStart.setHours(0, 0, 0, 0);
-    workEnd.setHours(0, 0, 0, 0);
-
-    if (daysDifference >= 0 && daysDifference < 6) {
-        const shiftStartHour = (daysDifference % 2) * 8 + 5;
-        const shiftEndHour = shiftStartHour + 8;
-        workStart.setHours(shiftStartHour, 0, 0, 0);
-        workEnd.setHours(shiftEndHour, 0, 0, 0);
-        if (today >= workStart && today <= workEnd) {
-            const timeLeft: number =
-                workEnd.getTime() - today.getTime() + 15 * 60 * 1000;
-            const hoursLeft: number = Math.floor(timeLeft / (1000 * 60 * 60));
-            const minutesLeft: number = Math.floor(
-                (timeLeft % (1000 * 60 * 60)) / (1000 * 60)
-            );
-            schedule += ` Il finit dans ${hoursLeft} heures et ${minutesLeft} minutes.`;
-        } else if (today < workStart) {
-            const timeLeft: number =
-                workStart.getTime() - today.getTime() - 30 * 60 * 1000;
-            const hoursLeft: number = Math.floor(timeLeft / (1000 * 60 * 60));
-            const minutesLeft: number = Math.floor(
-                (timeLeft % (1000 * 60 * 60)) / (1000 * 60)
-            );
-            schedule += ` Il s'en va dans ${hoursLeft} heures et ${minutesLeft} minutes.`;
-        }
     }
 
     return schedule;
